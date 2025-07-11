c4f24e51fc56dad3a3c36c0ce8d3a90d4335a116
Tauqeer-code-craft <tauqeerahmed90673@gmail.com>
2025-07-11 23:19:57 +0530

    Initial setup in frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import AdminDashBoard from './pages/Admin/AdminDashBoard'
 
 const App: React.FC = () => {
   return (
-    <div>App
-      <div><h1>helo from fronntend</h1>
-    
-      <h1></h1>
-      <h1></h1>
-      <h1></h1></div>
+    <div>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        
+        {/* //Admin Routes  */}
+        <Route element={<PrivateRoute allowedRoles={["admin"]}/>} >
+          <Route path='/admin/dashboard' element={<AdminDashBoard/>}></Route> 
+        </Route>
+      </Routes>
     </div>
 
   )
