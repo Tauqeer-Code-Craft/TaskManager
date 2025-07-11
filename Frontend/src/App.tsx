05ebcc67f167fcb3df6998427e3c132f1f560462
Tauqeer-code-craft <tauqeerahmed90673@gmail.com>
2025-07-11 23:42:08 +0530

    More Setup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,12 @@ import { Routes,Route } from 'react-router-dom'
 import Login from './pages/Auth/Login'
 import Signup from './pages/Auth/Signup'
 import AdminDashBoard from './pages/Admin/AdminDashBoard'
+import PrivateRoute from './routes/PrivateRoute'
+import ManageTasks from './pages/Admin/ManageTasks'
+import CreateTask from './pages/Admin/CreateTask'
+import UserDashBoard from './pages/User/UserDashBoard'
+import MyTasks from './pages/User/MyTasks'
+import ViewTaskdetails from './pages/User/ViewTaskdetails'
 
 const App: React.FC = () => {
   return (
@@ -14,6 +20,15 @@ const App: React.FC = () => {
         {/* //Admin Routes  */}
         <Route element={<PrivateRoute allowedRoles={["admin"]}/>} >
           <Route path='/admin/dashboard' element={<AdminDashBoard/>}></Route> 
+          <Route path='/admin/tasks' element={<ManageTasks/>}></Route> 
+          <Route path='/admin/create-task' element={<CreateTask/>}></Route> 
+        </Route>
+
+        {/* User Routes */}
+        <Route>
+          <Route path='/user/dashboard' element={<UserDashBoard/>} />
+          <Route path='/user/tasks' element={<MyTasks/>} />
+          <Route path='/user/task-details/:id' element={<ViewTaskdetails/>} />
         </Route>
       </Routes>
     </div>
---
+++ Frontend/src/pages/User/ViewTaskdetails.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewTaskdetails = () => {
+  return (
+    <div>ViewTaskdetails</div>
+  )
+}
+
+export default ViewTaskdetails
