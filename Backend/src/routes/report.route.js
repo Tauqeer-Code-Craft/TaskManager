56b461d2f73be489daae6a24cc406985e681bae2
Tauqeer-code-craft <tauqeerahmed90673@gmail.com>
2025-07-14 00:29:28 +0530

    report feature

--- Backend/src/routes/report.route.js
+++ Backend/src/routes/report.route.js
@@ -1,5 +1,10 @@
-import express from "express"
+import express from "express";
+import authMiddleWare from "../middlewares/auth.middleware.js";
+import reportController from "../controllers/report.controller.js";
 
 const router = express.Router()
+
+router.get("/export/tasks",authMiddleWare.protect,authMiddleWare.adminOnly,reportController.exportTasksReport)
+router.get("/export/users",authMiddleWare.protect,authMiddleWare.adminOnly,reportController.exportUsersReport)
 
 export default router
