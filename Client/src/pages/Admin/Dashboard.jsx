385bb89c998d74d2c0fe0d0be22b3ab5fea4221a
Tauqeer-code-craft <tauqeerahmed90673@gmail.com>
2025-07-23 22:32:20 +0530

    dashboard update

--- Client/src/pages/Admin/Dashboard.jsx
+++ Client/src/pages/Admin/Dashboard.jsx
@@ -5,7 +5,9 @@ import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
-import {moment} from 'moment'
+import { addThousandsSeperator } from '../../utils/helper';
+import moment from 'moment'
+import InfoCard from '../../components/Cards/InfoCard';
 
 const Dashboard = () => {
 
@@ -13,7 +15,7 @@ const Dashboard = () => {
   const { user} = useContext(UserContext);
   const navigate = useNavigate();
 
-  const [DashboardData , setDashboardData] = useState(null);
+  const [dashboardData , setDashboardData] = useState(null);
   const [pieChartData, setPieChartData] = useState([]);
   const [barChartData, setBarChartData] = useState([]);
 
@@ -44,12 +46,45 @@ const Dashboard = () => {
       <div className='card my-5'>
         <div>
           <div className='col-span-3'>
-            <h2 className='text-xl md:text-2xl'>Good Day! {user.name}</h2>
+            <h2 className='text-xl md:text-2xl'>Good Day! {user?.name}</h2>
             <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
               {moment().format("dddd Do MMM YYYY")}</p>
           </div>
         </div>
+
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5'>
+          <InfoCard
+            label= "Total Tasks"
+            value={addThousandsSeperator(
+              dashboardData?.charts?.taskDistribution?.All || 0
+            )}
+            color="bg-primary"
+          ></InfoCard>
+          <InfoCard
+            label= "Pending Tasks"
+            value={addThousandsSeperator(
+              dashboardData?.charts?.taskDistribution?.Pending || 0
+            )}
+            color="bg-violet-500"
+          ></InfoCard>
+          <InfoCard
+            label= "In Progress Tasks"
+            value={addThousandsSeperator(
+              dashboardData?.charts?.taskDistribution?.InProgress || 0
+            )}
+            color="bg-cyan-500"
+          ></InfoCard>
+          <InfoCard
+            label= "Completed Tasks"
+            value={addThousandsSeperator(
+              dashboardData?.charts?.taskDistribution?.Completed || 0
+            )}
+            color="bg-lime-500"
+          ></InfoCard>
+        </div>
       </div>
+
+      <div></div>
     </DasboardLayout>
   )
 }
